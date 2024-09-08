c987d2e69aeee1269d14cf59de81cdd3ff5bf21c
Linards <lincis20@inbox.lv>
2024-09-08 15:33:38 +0300

    Added Header and Hero sections

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,5 +1,26 @@
 // "use client";
+import { GrMenu } from "react-icons/gr";
 
 export const Header = () => {
-    return "Header";
+    return (
+        <header className="sticky top-0 backdrop-blur-sm z-20">
+            <div className="flex justify-center items-center py-3">
+                <div className="container">
+                    <div className="flex items-center justify-evenly">
+                        <span className="font-bold">FridgeFolio</span>
+                        <GrMenu className="h-5 w-5 md:hidden" />
+                        <nav className="hidden md:flex gap-6 items-center">
+                            <a href="#">Features</a>
+                            <a href="#">How It Works</a>
+                            <a href="#">Pricing</a>
+                            <a href="#">Testimonials</a>
+                        </nav>
+                        <button className="btn btn-primary hidden md:block">
+                            Try demo
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </header>
+    );
 };
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,5 +1,47 @@
+import Image from "next/image";
+import PlaceHolder from "@/assets/placeholder-website.webp";
+
 // "use client";
 
 export const Hero = () => {
-    return "Hero";
+    return (
+        <section className="pt-12 pb-20 md:pt-5 md:pb-10 flex items-center justify-center min-h-screen">
+            <div className="container">
+                <div className="max-w-5xl mx-auto">
+                    <div className="flex flex-col text-center md:pt-10">
+                        <div className="tag relative ps-6 inline-block mx-auto">
+                            <span className="absolute left-2 top-2.5 transform -translate-y-1/2 w-2 h-2 rounded-full bg-[#31ae4c] animate-custom-pulse"></span>
+                            Public beta out now
+                        </div>
+                        <h1 className="text-5xl md:text-7xl font-bold mt-6">
+                            Save Recipes, Plan Meals, and Stay Organized with
+                            FridgeFolio
+                        </h1>
+                        <p className="text-xl text-gray-400 mt-6">
+                            Discover the ultimate recipe-saving app that lets
+                            you effortlessly save recipes, adjust portion sizes,
+                            plan meals, and track nutrition. Whether you're a
+                            home cook or a pro chef, FridgeFolio helps you
+                            create delicious meals with ease
+                        </p>
+                        <div className="flex gap-1 items-center justify-center mt-[30px]">
+                            <button className="btn btn-primary">
+                                Try demo
+                            </button>
+                            <button className="btn btn-text">Learn more</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="mt-12 flex justify-center">
+                    <Image
+                        src={PlaceHolder}
+                        alt="FridgeFolio Website"
+                        width={900}
+                        height={675}
+                        className="w-auto h-auto border-4"
+                    />
+                </div>
+            </div>
+        </section>
+    );
 };
